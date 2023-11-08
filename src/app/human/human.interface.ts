6a893875d59a873ef2b9c9cdbc46315849d6c3d5
jamesyobe <jamesyobe@yahoo.com>
2023-11-08 13:28:35 -0600

    Update Human

---
+++ src/app/human/human.interface.ts
@@ -0,0 +1,11 @@
+export interface Paths {
+    style: string;
+    d: string;
+    id: string;
+}
+export interface Pathsdetails {
+    id: string;
+    fill: string;
+    stroke: string;
+    d: string;
+}
